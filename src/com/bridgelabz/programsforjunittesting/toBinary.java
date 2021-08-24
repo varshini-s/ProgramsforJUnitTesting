4c333f124bee57854b5e2264c9a1e5bd3e883a7d
varshini <dreamvarshini@gmail.com>
2021-08-24 20:14:45 +0530

    ADDED:Binary to decimal conversion code

---
+++ src/com/bridgelabz/programsforjunittesting/toBinary.java
@@ -0,0 +1,44 @@
+package com.bridgelabz.programsforjunittesting;
+
+import java.util.Scanner;
+
+public class toBinary 
+{
+
+	public static void main(String[] args) 
+	{
+		int power = 1;
+		
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter decimal value to convert to binary");
+        int inputDecimalNumber=scanner.nextInt();
+
+        while (power <= inputDecimalNumber/2) 
+        {
+            power *= 2;
+        }
+        
+  
+        while (power > 0) 
+        {
+
+            if (inputDecimalNumber < power) 
+            {
+                System.out.print(0);
+            }
+
+            else 
+            {
+                System.out.print(1);
+                inputDecimalNumber -= power;
+            }
+
+            power /= 2;
+        }
+
+       
+		
+
+	}
+
+}
