8742013ebc070e423e7d1f4418436373b86522d2
varshini <dreamvarshini@gmail.com>
2021-08-24 18:05:34 +0530

    ADDED:Vending machine code

---
+++ src/com/bridgelabz/programsforjunittesting/VendingMachine.java
@@ -0,0 +1,44 @@
+package com.bridgelabz.programsforjunittesting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class VendingMachine 
+{
+	public static int remainingChange=0;
+
+	public static int notes[]= {1000,500,100,50,10,5,2,1};
+	
+	public static int  minimumNotes(int totalChange,int index)
+	{
+		if(totalChange==0)
+			return 0;
+		
+		else if(totalChange>=notes[index])
+		{
+			remainingChange=totalChange-notes[index];
+			System.out.print(" "+notes[index]);
+			return 1+minimumNotes(remainingChange,index);
+		}
+		else
+		{
+			return 0+minimumNotes(totalChange,index+1);
+		}
+		
+	}
+
+	public static void main(String[] args) 
+	{
+		
+		 Scanner scanner = new Scanner(System.in);
+		
+		 System.out.println("Enter change to be return by vending machine");
+		 int change=scanner.nextInt();
+		 
+		 System.out.println("List of Rs notes given in change :");
+		 System.out.println("\nMinimum notes needed to give changes is:"+minimumNotes(change,0));
+		 
+
+	}
+
+}
